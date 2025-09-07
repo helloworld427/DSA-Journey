6c01d9d9983e0bcaeeb2d8dc79aa3337c350965d
hello world <singhsoun2001@gmail.com>
2025-09-07 23:57:51 +0530

    did palindrome checkin String

---
+++ Strings /Palindrome.java
@@ -0,0 +1,23 @@
+public class Palindrome{
+public static void main(String[] args){
+
+        //Method 1
+        // String name = "karak";
+        // String sb = new StringBuilder(name).reverse().toString();
+        // if(sb.equals(name)) System.out.println("yes");
+        // else System.out.println("no");
+
+        //Method 2
+        // String name = "karrak";
+        // int i=0, j=name.length()-1;
+        // while(i<j){
+        //     if(name.charAt(i)!=name.charAt(j)){
+        //         break;
+        //     }
+        //     i++;
+        //     j--;
+        // }
+        // if(j<=i) System.out.println("yes");
+        // else System.out.println("No");
+    }
+}
