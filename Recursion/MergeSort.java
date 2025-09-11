db00aedd947b6ec613e4c1d9029c95c857b6dea6
hello world <singhsoun2001@gmail.com>
2025-09-11 09:49:49 +0530

    merge Sort

---
+++ Recursion/MergeSort.java
@@ -0,0 +1,51 @@
+
+package Recursion;
+
+import java.util.*;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 3, 2, 1 };
+        int[] narr = mS(arr);
+        System.out.println(Arrays.toString(narr));
+    }
+
+    static int[] mS(int[] arr) {
+        if (arr.length == 1)
+            return arr;
+
+        int mid = arr.length / 2;
+
+        int[] left = mS(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mS(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+
+    static int[] merge(int[] left, int[] right) {
+        int[] mix = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                mix[k] = left[i];
+                i++;
+            } else {
+                mix[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<left.length){
+            mix[k] = left[i];
+            i++;
+            k++;
+        }
+         while(j<right.length){
+            mix[k] = right[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+
+}
